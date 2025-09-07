2a641288a99259963390be1159016a7b1f2953e9
Devendra Kumar Dubey <146335880+Devcodes24@users.noreply.github.com>
2025-09-07 15:34:10 +0530

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,13 +15,4 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })
-    ->create();
-
-// Redirect storage + cache to writable /tmp on Vercel
-if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
-    $app->useStoragePath('/tmp/storage');
-    $app->useBootstrapPath('/tmp/bootstrap');
-}
-
-return $app;
+    })->create();
